4cf8df5dc3ed2f99abf027132561b2564767f8cc
Carlos <131690161+Yosoyina@users.noreply.github.com>
2025-01-13 20:13:36 +0100

    Copy Paste del Main
    aqui copio i pego el codigo de main de la flor

--- src/SimpleBakingSystem/Main.java
+++ src/SimpleBakingSystem/Main.java
@@ -5,6 +5,28 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Account myAccount;
+        
+	    myAccount = new Account("Flor Martinez", "1000-1234-56-123456789", 2500);
+	     
+	    try {
+	    	myAccount.withdrawAmount(2300);
+	    } catch(Exception e){
+        	System.err.println(e.getMessage());
+            System.out.println("Error al retirar");
+	    }
+	     
+	    try {
+	    	 System.out.println("Ingrés al compte");
+	    	 myAccount.depositAmount(1695);
+	    } catch(Exception e){
+	    	 System.err.println(e.getMessage());
+	         System.out.println("Error en l'ingrés");
+	    }
+
+	     System.out.println("El saldo actual es " + myAccount.getBalance());   
+		
+		
 	}
 
 }
